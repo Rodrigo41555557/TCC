be76b179a17d26f80072266bb83967877768a6e8
Future Man 623 <37418306+Rodrigo41555557@users.noreply.github.com>
2019-09-11 08:43:07 -0300

    Update editar_user.php

--- editar_user.php
+++ editar_user.php
@@ -1,47 +1,6 @@
 
 <?php
 require 'config.php';
-
-if(empty($_SESSION['apelido'])){
-		header('Location: login.php');
-
-	if(isset($_POST['save'])) {
-		$errMsg = '';
-
-		// Getting data from FROM
-		$nome = $_POST['nome_completo'];
-		$senha = $_POST['senha'];
-		$confSenha = $_POST['confsenha'];
-        $telefone = $_POST['telefone'];
-
-		if($senha != $confSenha){
-			$errMsg = 'Senha Incorreta.';
-        }
-
-		if($errMsg == '') {
-			try {
-		      $sql = 'UPDATE usuario SET nome_completo = :nome_completo, senha = :senha, telefone = :telefone WHERE apelido = :apelido and cod_usuario = '.$_SESSION['id'];
-		      $stmt = $connect->prepare($sql);                                  
-		      $stmt->execute(array(
-		        ':nome_completo' => $nome_completo,
-		        ':senha' => $senha,
-                ':telefone' => $telefone,
-		        ':apelido' => $_SESSION['apelido']
-		      ));
-				header('Location: editar_user.php?action=alterado');
-				exit;
-
-			}
-			catch(PDOException $e) {
-				$errMsg = $e->getMessage();
-			}
-		}
-	}
-
-	if(isset($_GET['action']) && $_GET['action'] == 'alterado'){
-		$errMsg = 'Usuário alterado com sucesso!';
-    }
-}
 ?>
 
 <!DOCTYPE html>
@@ -61,16 +20,75 @@ if(empty($_SESSION['apelido'])){
 <div id="conteudo" class="contanier">
 	<form method="post" action="">
 		
+<?php
+
+if(empty($_SESSION['id'])){
+        header('Location: login.php');
+
+    if(isset($_POST['save'])) {
+        $errMsg = '';
+
+        // Getting data from FROM
+        $nome = $_POST['name'];
+        echo '<div class="col-md-6 offset-md-3">
+                            <h5 class="text-danger text-center">'.$nome.'</h5>
+                       </div>.';
+        $senha = $_POST['senha'];
+        $confSenha = $_POST['confsenha'];
+        $telefone = $_POST['telefone'];
+
+        if($senha != $confSenha)
+            $errMsg = '<div class="col-md-6 offset-md-3">
+                            <h5 class="text-danger text-center"> Senha Incorreta </h5>
+                       </div>.';
+        
+
+        if($errMsg == '') {
+            try {
+              $sql = "UPDATE usuario SET nome_completo = :nome_completo, senha = :senha, telefone = :telefone WHERE `usuario`.`cod_usuario` = ".$_SESSION['id'];
+              $stmt = $connect->prepare($sql);                                  
+              $stmt->execute(array(
+                ':nome_completo' => $nome,
+                ':senha' => $senha,
+                ':telefone' => $telefone,
+                ':apelido' => $_SESSION['apelido']
+              ));
+                header('Location: editar_user.php?action=alterado');
+                exit;
+
+            }
+            catch(PDOException $e) {
+                $errMsg = $e->getMessage();
+            }
+        }
+    }
+
+    if(isset($_GET['action']) && $_GET['action'] == 'alterado')
+        $errMsg = 'Usuário alterado com sucesso!';
+    
+}
+?>		
+		
 		<div class="form-group">
         	<div class="col-md-6 offset-md-3">
 	        	<h3 class="text-center"> Alterar Usuário </h3>
 	    	</div>
     	</div>
 
+        <?php
+                if(isset($errMsg)){
+                    echo '<div class="form-group">
+                                <div class="col-md-6 offset-md-3">
+                                    <h3 class="text-center">' .$errMsg. '</h3>
+                                </div>
+                          </div>';
+                }
+        ?>
+
         <div class="form-group">
             <div class="col-md-6 offset-md-3">
                 <label > Apelido </label>
-                <input type="text" name="apelido" class="form-control" placeholder="Apelido" required="" >
+                <input type="text" name="apelido" class="form-control" placeholder="<?= $_SESSION['apelido'] ?>" required="" >
             </div>
         </div>
 
@@ -78,14 +96,14 @@ if(empty($_SESSION['apelido'])){
         <div class="form-group">
             <div class="col-md-6 offset-md-3">
                 <label> Nome Completo </label>  
-                <input type="text" name="nome_completo" class="form-control" placeholder="Nome Completo" required="" >
+                <input type="text" name="nome_completo" class="form-control" placeholder="<?= $_SESSION['name'] ?>" required="" >
             </div>
         </div>
 
         <div class="form-group">
             <div class="col-md-6 offset-md-3">
                 <label> Número de Telefone </label>  
-                <input type="tel" name="telefone" class="form-control" placeholder="Telefone" required="" >
+                <input type="tel" name="telefone" class="form-control" placeholder="<?= $_SESSION['telefone'] ?>" required="" >
             </div>
         </div>
 
@@ -112,7 +130,6 @@ if(empty($_SESSION['apelido'])){
         </div>
 
 	</form>
-<br><br><br>
 </div>
 <?php include 'rodape.php'; ?>
 </body>
