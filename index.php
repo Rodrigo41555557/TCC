3a836bf6b2acd122d55fc43ce9608fecab0ed1cb
Rodrigo41555557 <rodrigoiag797@gmail.com>
2019-10-16 09:18:32 -0300

    .

--- index.php
+++ index.php
@@ -8,32 +8,46 @@ require 'config.php';
     <title>Ouvindo Você!</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body class="bg-light">
 
 <?php include 'menu.php'; ?>	
 
 <br><br><br>
-
 <div class="container">       
             
         <div id="opcoes" class="row">          
             
-            <div id="resenha" class="col-sm-4 mb-5">
+
+            <div id="resenha" class="elemento col-sm-3 mb-4">
                 <div  class="card">
                     <a href="elogio.php"><img class="card-img" src="img/elogio.jpg" height="150px"></a>
                 </div>
+                <div class="titulo">
+                    Elogio
+                </div>
             </div>
 
-			<div id="resenha" class="col-sm-4 mb-5">
+            <div id="resenha" class="col-sm-1 mb-1">
+                   <article class="bg-dark"></article>
+            </div>
+
+			<div class="elemento col-sm-3 mb-4">
                 <div  class="card">
                     <a href="sugestao.php"><img class="card-img" src="img/sugestao.jpg" height="150px" ></a>
                 </div>
+                <div class="titulo">
+                    Sugestao
+                </div>
             </div>
-            
-            <div id="resenha" class="col-sm-4 mb-5">
+
+            <div id="resenha" class="col-sm-1 mb-1">
+                   <article class="bg-dark"></article>
+            </div>
+
+            <div id="resenha" class="elemento col-sm-3 mb-4">
                 <div  class="card">
                     <a href="informacao.php"><img class="card-img" src="img/informacao.jpg" height="150px" ></a>
                 </div>
