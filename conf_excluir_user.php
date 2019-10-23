b41e935689b8337c61dfebeda30677e155ac3bdd
Rodrigo41555557 <rodrigoiag797@gmail.com>
2019-10-23 09:26:54 -0300

    .

--- conf_excluir_user.php
+++ conf_excluir_user.php
@@ -3,13 +3,10 @@ if($errMsg == ''){
 
 	$id = $_SESSION['id'];				 
 
-	function deletar($iden)
-	{
-		$sql = "DELETE FROM `usuario` WHERE `usuario`.`cod_usuario` = $iden";  
-		$q = $connect->prepare($sql);
-		$response = $q->execute(array($iden)); 
-	}
-	deletar($id);
+	$sql = "DELETE FROM `usuario` WHERE `usuario`.`cod_usuario` = $id";  
+	$q = $connect->prepare($sql);
+	$response = $q->execute(array($id)); 
+
 	session_destroy();
 	header('Location: login.php?action=deletado');   
 }
