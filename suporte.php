b4d4f3a5fe60f3e386e4e0b7789bb12622b89cbf
Rodrigo da Silva <37418306+Rodrigo41555557@users.noreply.github.com>
2019-11-05 16:23:04 -0300

    Update suporte.php

--- suporte.php
+++ suporte.php
@@ -21,7 +21,7 @@
 	<div class="container">  
 		<h1>Ouvindo Você</h1>
 		<br>
-		<p>Bem vindo, O Ouvindo Você é um sistema de ouvidoria que tem como objetivo auxiliar as pessoas que querem expor suas opiniões e críticas de forma simples, rápida e prática sobre algum órgão público ou privado.</p>
+		<p>O Ouvindo Você é um sistema de ouvidoria que tem como objetivo auxiliar as pessoas que querem expor suas opiniões e críticas de forma simples, rápida e prática sobre algum órgão público ou privado. Os usuários que quiserem postar algo  dentro do site terão a opção de escolher o tipo de postagem que acharem melhor para eles, sendo as opções de postagem anônima e não anônima. Essa funcionalidade existe porque várias pessoas não se sentem cômodas em expressar sua opinião ou uma reclamação quando seu nome pode ser visto e sua identidade revelada por qualquer outra pessoa que acesse o site, desse modo, o sistema dá essa opção para que pessoas possam expressar seus pensamentos sem risco de serem reconhecidas. Todas as postagens serem gravadas no sistema e terão uma página específica para que possam ser visualizadas.  </p>
 		<br>
 		<h5>Segue emails para contato caso precise de ajuda</h5>
 		<br>
